2215f8ea1a96710ffbc5b2f77be0e86343c2aeff
jaydipjadhav <jaydipdjadhav>
2024-07-26 17:51:49 +0530

    write Postcard , select , signin , signup , Input componentes

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,19 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogOutBtn from "./header/LogOutBtn";
+import Button from "./Button";
+import Input from "./Input"
+
+
+
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogOutBtn
+    LogOutBtn,
+    Button,
+    Input
 
 }
